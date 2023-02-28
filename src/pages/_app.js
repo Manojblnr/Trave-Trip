5da514ee2f1ede696c5f3dcee000db3921b5e661
unknown <manojblnr25@gmail.com>
2023-02-28 14:15:50 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,41 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { Container } from '@mui/system';
+import Navbar from 'components/Navbar';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+const theme = createTheme({
+  palette: {
+    text: {
+      primary:'#708238'
+    },
+    primary:{
+      main:'#fff'
+    }
+  },
+  typography: {
+    fontFamily:'Poppins',
+    h2: {
+      fontFamily:'Alex Brush'
+    },
+    h3: {
+      fontFamily:'Alex Brush'
+    }
+
+  }
+})
+
+
+
+function MyApp({ Component, pageProps }) {
+  return <ThemeProvider theme={theme}>
+    <CssBaseline/>
+    <Navbar/>
+    <Container maxWidth='lg'>
+      <Component {...pageProps} />
+   </Container>
+   </ThemeProvider>
 }
+
+export default MyApp
---
+++ src/pages/trip/[slug].js
@@ -0,0 +1,97 @@
+import { Stack, Typography,Box, Skeleton } from "@mui/material";
+import Image from "next/legacy/image";
+import React from "react";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { useRouter } from 'next/router';
+import client from "config/contentful";
+
+export const getStaticPaths = async () => {
+  const response = await client.getEntries({
+    content_type: "trips",
+  });
+
+
+  const paths = response.items.map((trip) => {
+    return {
+      params: {
+        slug: trip.fields.slug,
+      },
+    };
+  });
+
+  console.log("Paths");
+  console.log(paths);
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  console.log("Params");
+  console.log(params);
+  const response = await client.getEntries({
+    content_type: "trips",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: {
+      trip: response.items[0],
+    },
+    revalidate: 1
+  };
+};
+
+const TripDetail = ({ trip }) => {
+  console.log(trip);
+
+  const router = useRouter()
+
+  if (router.isFallback) {
+    console.log('showing fallback page')
+    return <Stack alignItems='center' spacing={2} mb={10}>
+      <Skeleton width='75%' height="400px" variant="rectangular" animation="wave"></Skeleton>
+      <Stack width='75%' >
+        <Typography variant="h1" width='75%'><Skeleton /></Typography>
+        <Typography variant="h3" width='35%'><Skeleton /></Typography>
+        <Typography variant="h4" width='55%'><Skeleton /></Typography>
+      </Stack>
+    </Stack>
+  }
+
+  const { title, brief, contentImage, attractions, description } = trip.fields;
+
+  
+  return (
+    <Stack spacing={5} mb={10}>
+      <Image
+        src={`https:${contentImage.fields.file.url}`}
+        alt="Content Image"
+        width={1200}
+        height={600}
+        layout='responsive'
+      />
+      <Stack spacing={2}>
+        <Typography variant="h4" fontWeight='bold'>{title}</Typography>
+        <Typography variant="h6" color='#7c7f7c'>{brief}</Typography>
+      </Stack>
+      <Stack spacing={2}>
+        <Typography variant="h5" fontWeight='500' >Attractions:</Typography>
+        <Stack direction='row'>
+          {
+            attractions && attractions.map((attraction,index) => {
+              return <Typography variant="subtitle1" key={index} color='#7c7f7c'>{`${attraction},`}</Typography>
+            })
+          }
+        </Stack>
+      </Stack>
+      <Stack spacing={2}>
+        <Typography variant="h5" fontWeight='500'>Description:</Typography>
+        <Box>{documentToReactComponents(description)}</Box>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default TripDetail;
